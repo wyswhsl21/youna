e02bfb0b32f684ddea37618562f5d6cdc64bdf3c
kimjaewoo <wyswhsl21@naver.com>
2023-02-08 16:38:23 +0900

    영어 api -> 한글로 변경

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 
-const Calculator = () => {
+const Calculator = ({ transfer }) => {
   const [info, setInfo] = useState([]);
   const [input, setInput] = useState("1");
-  const [exchangeInput, setExchangeInput] = useState(1 / 1256);
+  const [exchangeInput, setExchangeInput] = useState((1 / 1256).toFixed(5));
   const [exchange, setExchange] = useState();
   const [dropdown, setDropdown] = useState([""]);
   const [seDropdown, setSeDropdown] = useState([""]);
   const [country, setCountry] = useState([]);
-  const [value, setValue] = useState(["KRW"]);
-  const [secondValue, setSecondValue] = useState(["USD"]);
+  const [value, setValue] = useState("KRW");
+  const [secondValue, setSecondValue] = useState("USD");
 
   useEffect(() => {
     async function getCurrency() {
@@ -22,66 +22,91 @@ const Calculator = () => {
 
       setDropdown(Object.entries(data.rates).map((item) => item[0]));
     }
-    console.log(input);
 
     getCurrency();
   }, []);
+  console.log(dropdown);
 
   //input change Handler
   const inputHandleChange = (e) => {
     setInput(e.target.value);
-    setExchangeInput((e.target.value / country[value]) * country[secondValue]);
+    setExchangeInput(
+      ((e.target.value / country[value]) * country[secondValue]).toFixed(5)
+    );
     console.log("dropdown ::", dropdown);
   };
 
   // 위에 select 박스 onchangeHandler
   const selectHandleChange = (e) => {
     setValue(e.target.value);
-    setExchangeInput((input / country[e.target.value]) * country[secondValue]);
+    setExchangeInput(
+      ((input / country[e.target.value]) * country[secondValue]).toFixed(5)
+    );
   };
+  console.log(value);
 
   //아래 input change handler
   const seInputChangeHandler = (e) => {
     let data = e.target.value;
     setExchangeInput(data);
-    setInput(data / country[secondValue]);
+    setInput(((data / country[secondValue]) * country[value]).toFixed(5));
   };
   // 아래 select 박스 onchangeHandler
   const selectExchangeHandler = (e) => {
     setSecondValue(e.target.value);
-    setExchangeInput((input / country[value]) * country[e.target.value]);
+    setExchangeInput(
+      ((input / country[value]) * country[e.target.value]).toFixed(5)
+    );
   };
-  console.log(country);
+  console.log(exchangeInput);
 
   return (
     <>
       <Header>
         <CalDiv>
-          <CalInput name="amount" value={input} onChange={inputHandleChange} />
+          <CalInput
+            name="amount"
+            value={input === "NaN" ? "" : input}
+            onChange={inputHandleChange}
+          />
           <select
             value={value}
             onChange={(e) => selectHandleChange(e)}
-            style={{ width: "300px", height: "50px" }}
+            style={{
+              outline: "none",
+              width: "130px",
+              height: "50px",
+              border: "none",
+              borderRadius: "0 10px 10px 0",
+            }}
           >
             {dropdown.map((item, index) => (
               <option key={index} value={item}>
-                {item}
+                {transfer?.map((trans) => trans[item])}
               </option>
             ))}
           </select>
         </CalDiv>
         <ExchangeDiv>
           <ExchangeInput
-            value={exchangeInput}
+            value={exchangeInput === "NaN" ? "" : exchangeInput}
             onChange={seInputChangeHandler}
           />
           <select
             value={secondValue}
             onChange={(e) => selectExchangeHandler(e)}
-            style={{ width: "300px", height: "50px" }}
+            style={{
+              outline: "none",
+              width: "130px",
+              height: "50px",
+              border: "none",
+              borderRadius: "0 10px 10px 0",
+            }}
           >
             {dropdown.map((item, index) => (
-              <option key={index}>{item}</option>
+              <option key={index} value={item}>
+                {transfer?.map((trans) => trans[item])}
+              </option>
             ))}
           </select>
         </ExchangeDiv>
@@ -95,6 +120,7 @@ const Header = styled.div`
   margin: auto;
   width: 100vw;
   height: 100vh;
+  gap: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -103,22 +129,39 @@ const Header = styled.div`
 const CalInput = styled.input`
   width: 100px;
   height: 45px;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+  border-right: 1px solid gray;
+  border-radius: 10px 0 0 10px;
 `;
 const ExchangeInput = styled.input`
   width: 100px;
   height: 45px;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+  border-right: 1px solid gray;
+  border-radius: 10px 0 0 10px;
 `;
 const CalDiv = styled.div`
   width: 300px;
-  height: 80px;
-  background-color: none;
-  border: black;
+  height: 50px;
+
+  border: 1px solid gray;
+  border-radius: 10px;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
 `;
 const ExchangeDiv = styled.div`
   width: 300px;
-  height: 80px;
+  height: 50px;
   display: flex;
   flex-direction: row;
+  border-radius: 10px;
+  border: 1px solid gray;
+  justify-content: space-between;
 `;
