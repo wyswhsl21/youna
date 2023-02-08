3ede11c5a910a2b82620a8883fbb7143a0d1b781
kimjaewoo <wyswhsl21@naver.com>
2023-02-08 17:50:29 +0900

    console.log 뜨지 않게 수정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Router from "./Shared/Router";
 import React from "react";
 
 const App = () => {
+  if (1 === 1) {
+    console.log = function no_console() {};
+    console.warn = function no_console() {};
+  }
   return (
     <div>
       <Router />
